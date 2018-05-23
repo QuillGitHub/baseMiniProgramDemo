d8637a1ae81afd1bcdbb1e7674285b332c334a4a
zhihesi <2302521258@qq.com>
2018-05-23 09:12:32 +0800

    initial

---
+++ pages/movable-area/movable-area.js
@@ -0,0 +1,19 @@
+// pages/movable-area/movable-area.js
+Page({
+  data: {
+    x: 0,
+    y: 0
+  },
+  tap: function (e) {
+    this.setData({
+      x: 30,
+      y: 30
+    });
+  },
+  onChange: function (e) {
+    console.log(e.detail)
+  },
+  onScale: function (e) {
+    console.log(e.detail)
+  }
+})
